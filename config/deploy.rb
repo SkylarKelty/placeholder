3f37fdc8ac5c90589bdfa12e1839bdac4b1bc99a
Skylar Kelty <skylarkelty@gmail.com>
2013-12-06 07:51:54 +0000

    Add more Cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,18 @@
+# Multi Stage Deploy
+require 'capistrano/ext/multistage'
+set :stages, %w(staging)
+set :default_stage, "staging"
+
+# App info
 set :application, "Placeholder App"
-set :repository,  "set your repository location here"
-
+set :repository,  "git@github.com:SkylarKelty/placeholder.git"
 set :scm, :git
+set :copy_exclude, ["Capfile", "config", "LICENSE", "README.md"]
 
-role :web, "your web-server here"
+# Deploy info
+set :user, "wwwdeploy"
+set :use_sudo, false
 
 # Clean up old releases on each deploy
+set :keep_releases, 3
 after "deploy:restart", "deploy:cleanup"
---
+++ config/deploy/staging.rb
@@ -0,0 +1,4 @@
+role :web, "Server Here"
+
+set :current_path, "/var/www/html/"
+set :releases_path, "/var/www/releases/"
